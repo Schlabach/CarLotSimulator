997e2f6d252045cfa3ad7fea617af681e34ece6d
Schlabach <matthewshawn89@gmail.com>
2023-01-03 16:10:59 -0500

    initial commit

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -13,6 +13,45 @@ namespace CarLotSimulator
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
+            CarLot lot = new CarLot();
+
+            var truck = new Car();
+            lot.CarList.Add(truck);
+            truck.Year = 2015;
+            truck.Make = "Chevy";
+            truck.Model = "Silverado";
+            truck.EngineNoise = "Vroom";
+            truck.HonkNoise = "Ahooga";
+            truck.IsDrivable = true;
+
+            truck.MakeHonkNoise();
+            truck.MakeEngineNoise();
+
+            var van = new Car();
+            lot.CarList.Add(van);
+            van.Year = 2018;
+            van.Make = "Honda";
+            van.Model = "Odyssey";
+            van.EngineNoise = "Rrrrr";
+            van.HonkNoise = "Beep Beep";
+            van.IsDrivable = true;
+
+            van.MakeHonkNoise();
+            van.MakeEngineNoise();
+
+            var suv = new Car();
+            lot.CarList.Add(suv);
+            suv.Year = 2020;
+            suv.Make = "Toyota";
+            suv.Model = "Highlander";
+            suv.EngineNoise = "Brrrr";
+            suv.HonkNoise = "Honk";
+            suv.IsDrivable = false;
+
+            suv.MakeHonkNoise();
+            suv.MakeEngineNoise();
+
+
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
@@ -23,6 +62,11 @@ namespace CarLotSimulator
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
             //*************BONUS X 2*************//
+
+            foreach(var car in lot.CarList)
+            {
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model}");
+            }
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
